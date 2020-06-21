c8abb803ab909a9231bd35751ce0ce53f8d79e3d
yokki12 <yokkiyom@gmail.com>
2020-06-21 10:12:13 +0900

    vending machine refactoring

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -3,19 +3,20 @@ require './lib/drink'
 class VendingMachine
 
   def initialize
-    @stocks = []
+    @stocks = {}
     5.times do
-      @stocks << Drink.cola
+      drink = Drink.cola
+      unless @stocks[drink.name]
+        @stocks[drink.name] = []
+      end
+      @stocks[drink.name] << drink
     end
   end
 
   def current_stocks
-    drink = @stocks[0]
-    [{name: drink.name, price: drink.price, stock: @stocks.count}]
+    @stocks.map do |name, drinks|
+      drink = drinks.first
+      {name: drink.name, price: drink.price, stock: drinks.count}
+    end
   end
 end
-
-# step1
-# 自動販売機は値段と名前の属性からなるジュースを１種類格納できる。
-# 初期状態で、コーラ（値段:120円、名前”コーラ”）を5本格納している。
-# 自動販売機は格納されているジュースの情報（値段と名前と在庫）を取得できる。
